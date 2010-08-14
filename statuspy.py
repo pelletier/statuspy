1094dbc075d8f7987076a57e0eb8e410be90209c
Thomas Pelletier <pelletier.thomas@gmail.com>
2010-08-14 13:55:10 +0200

    user registration done

--- statuspy.py
+++ statuspy.py
@@ -1,16 +1,24 @@
 import json
+import hashlib
 import redis
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import settings
 
-API_VERSION = settings.API_VERSION # Fort shortcut (see app)
 
+API_VERSION = settings.API_VERSION # Fort shortcut (see app)
 
 # Create a pooled connection right now, so we don't waste time connecting 
 # sockets on the run.
-redis_connection = redis.Redis(host=settings.REDIS_HOST)
+r = None
+if not settings.DEBUG:
+    r = redis.Redis(host=settings.REDIS_HOST, db=settings.REDIS_DB)
+else:
+    r = redis.Redis(host=settings.REDIS_HOST, db=settings.REDIS_DB_DEBUG)
+
+# Create an MD5 hash object, better in case of massive subscription / login
+md5 = hashlib.md5()
 
 
 # Handle the web UI ----------------------------------------------------------
@@ -34,17 +42,43 @@ class APIBaseHandler(tornado.web.RequestHandler):
 
 # --> User managmenent
 class APIUsersHandler(tornado.web.RequestHandler):
-
+    
     def get(self, user_name):
         if not user_name:
             self.write({'statuspy':'Welcome', 'version':API_VERSION})
             return None
-
+    
     def post(self, user_name):
         if user_name:
             raise tornado.web.HTTPError(405)
         
+        # Gather needed data
+        try:
+            user_name = self.request.arguments['user_name'][0]
+            password = self.request.arguments['password'][0]
+            email = self.request.arguments['email'][0]
+        except KeyError:
+            raise tornado.web.HTTPError(400)
         
+        print password
+        
+        # Check if user name does not already exists
+        if r.get('username:%s:uid' % user_name):
+            raise tornado.web.HTTPError(409)
+        
+        # Hash the password
+        md5.update(password)
+        hashed_password = md5.hexdigest()
+        
+        # Let's inscrement the global user id to get a new one
+        uid = r.incr('global:nextUserId')
+        
+        r.set('username:%s:uid' % user_name, uid)
+        r.set('uid:%s:username' % uid, user_name)
+        r.set('uid:%s:password' % uid, hashed_password)
+        r.set('uid:%s:email' % uid, email)
+        
+        self.write({'uid': uid})
 
 
 # Tornado application
--- tests.py
+++ tests.py
@@ -1,12 +1,19 @@
-import unittest
-import restkit
+import json
+import redis
 from tornado.testing import *
 import statuspy
+import settings
 from settings import API_VERSION
+
+settings.DEBUG = True
+
+# We go on fresh tests
+r = redis.Redis(host=settings.REDIS_HOST, db=settings.REDIS_DB_DEBUG)
+r.flushdb()
 
 
 class APIUsersTest(AsyncHTTPTestCase):
-
+    
     def get_app(self):
         return statuspy.application
 
@@ -17,6 +24,35 @@ class APIUsersTest(AsyncHTTPTestCase):
         self.assertEqual(response.body, '{"version": "%s", "statuspy": "Welcome"}' % API_VERSION)
 
     def test_post_with_user_name(self):
-        self.http_client.fetch(self.get_url('/%s/foo' % API_VERSION), self.stop, method="POST", body='')
+        self.http_client.fetch(self.get_url('/%s/foo' % API_VERSION),\
+                               self.stop, method="POST", body='')
         response = self.wait()
         self.assertEqual(response.code, 405)
+    
+    def test_post_missing_arg(self):
+        self.http_client.fetch(self.get_url('/%s/' % API_VERSION),\
+                               self.stop, method="POST",\
+                               body='user_name=bob')
+        response = self.wait()
+        self.assertEqual(response.code, 400)
+    
+    def test_post_full_args(self):
+        self.http_client.fetch(self.get_url('/%s/' % API_VERSION),
+                               self.stop, method='POST',\
+                               body='user_name=bob&password=alice&email=aaa@example.com')
+        response = self.wait()
+        self.assertEqual(response.code, 200)
+        data = json.loads(response.body)
+        self.assertTrue(data.get('uid'))
+    
+    def test_post_already_exists(self):
+        self.http_client.fetch(self.get_url('/%s/' % API_VERSION),
+                               self.stop, method='POST',\
+                               body='user_name=guy&password=yeah&email=bbb@example.com')
+        response = self.wait()
+        self.http_client.fetch(self.get_url('/%s/' % API_VERSION),
+                               self.stop, method='POST',\
+                               body='user_name=guy&password=yeah&email=bbb@example.com')
+        response = self.wait()
+        self.assertEqual(response.code, 409)
+        
